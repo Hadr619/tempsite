1afffc502a2b935b8491cf4baef4abdbea20f7a7
Perry Reid <notownpappa@gmail.com>
2018-01-23 19:44:40 -0800

    Npm init, setting up base trippy lines, initial repo

---
+++ README.md
@@ -0,0 +1,8 @@
+# TEMP SITE
+
+## a quick site to take down my current portfolio
+
+```
+$ git clone $project  
+$ npm install
+```
---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+'use-strict';
+
+let gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	sass = require('gulp-sass'),
+	sourcemaps = require('gulp-sourcemaps'),
+	del = require('del');
+
+gulp.task('concatScripts',['clean'], ()=> {
+return gulp.src('./js/app/*.js')
+	.pipe(sourcemaps.init())
+	.pipe(concat('site.js'))
+	.pipe(sourcemaps.write('./'))
+	.pipe(gulp.dest('js'))
+});
+
+gulp.task('minifyScripts', ['concatScripts'], ()=>{
+return gulp.src('js/site.js')
+	.pipe(uglify())
+	.pipe(rename('site.min.js'))
+	.pipe(gulp.dest('js'))
+});
+
+gulp.task('sass', ()=>{
+return gulp.src('scss/site.scss')
+	.pipe(sourcemaps.init())
+	.pipe(sass())
+	.pipe(sourcemaps.write('./'))
+	.pipe(gulp.dest('css'))
+});
+
+
+gulp.task('watchFiles', ()=>{
+	gulp.watch('scss/**/*.scss', ['sass']);
+	gulp.watch('js/**/*.js', ['concatScripts']);
+});
+
+gulp.task('clean', ()=>{
+	del(['dist', 'css/site.css*', 'js/site*.js*']);
+})
+
+gulp.task('build',['minifyScripts', 'sass'], ()=>{
+	return gulp.src(["css/site.css", "js/site.min.js", 'index.html',
+						"img/**", "fonts/**"], { base: './'})
+			.pipe(gulp.dest('dist'));
+});
+
+gulp.task('serve', ['watchFiles']);
+
+
+gulp.task('default', ['clean'], ()=>{
+	gulp.start('build');
+});
